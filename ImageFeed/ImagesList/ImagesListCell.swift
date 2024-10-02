77af649428ab64e19cc3ff497285ea39a37efee6
Symbat Salin <symbatsalin@gmail.com>
2024-10-02 07:22:44 +0500

    Created gradientView and layer

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -2,12 +2,46 @@ import UIKit
 
 final class ImagesListCell: UITableViewCell {
     static let reuseIdentifier = "ImagesListCell"
+    private var gradientLayer: CAGradientLayer?
+    
+    
+    @IBOutlet private weak var gradientView: UIView!
     
     @IBOutlet private weak var cellImage: UIImageView!
     
     @IBOutlet private weak var likeButton: UIButton!
     
     @IBOutlet private weak var dateLabel: UILabel!
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        setupGradient()
+    }
+    
+    private func setupGradient() {
+        if gradientLayer == nil {
+            gradientLayer = CAGradientLayer()
+            
+            gradientLayer?.colors = [
+                UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0).cgColor,
+                UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.2).cgColor
+            ]
+            
+            gradientLayer?.startPoint = CGPoint(x: 0.5, y: 0.0)
+            gradientLayer?.endPoint = CGPoint(x: 0.5, y: 1.0)
+            
+            gradientLayer?.frame = gradientView.bounds
+            
+            gradientView.layer.addSublayer(gradientLayer!)
+            gradientView.bringSubviewToFront(dateLabel)
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        gradientLayer?.removeFromSuperlayer()
+        gradientLayer = nil
+    }
     
     func configure(image: UIImage?, date: String, isLiked: Bool) {
         cellImage.image = image
@@ -16,5 +50,6 @@ final class ImagesListCell: UITableViewCell {
         let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "No Active")
         likeButton.setImage(likeImage, for: .normal)
     }
+    
 }
 
