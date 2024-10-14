02051f6a7b0c5012d5908860a4664f982015cf66
Symbat Salin <symbatsalin@gmail.com>
2024-10-14 08:09:25 +0500

    Few fixes

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -15,26 +15,24 @@ final class ImagesListCell: UITableViewCell {
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        setupGradient()
+        gradientLayer?.frame = gradientView.bounds
     }
     
     private func setupGradient() {
-        if gradientLayer == nil {
-            gradientLayer = CAGradientLayer()
-            
-            gradientLayer?.colors = [
-                UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0).cgColor,
-                UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.2).cgColor
-            ]
-            
-            gradientLayer?.startPoint = CGPoint(x: 0.5, y: 0.0)
-            gradientLayer?.endPoint = CGPoint(x: 0.5, y: 1.0)
-            
-            gradientLayer?.frame = gradientView.bounds
-            
-            gradientView.layer.addSublayer(gradientLayer!)
-            gradientView.bringSubviewToFront(dateLabel)
-        }
+        guard gradientLayer == nil else { return }
+
+        gradientLayer = CAGradientLayer()
+
+        gradientLayer?.colors = [
+            UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0).cgColor,
+            UIColor(red: 26/255, green: 27/255, blue: 34/255, alpha: 0.2).cgColor
+        ]
+
+        gradientLayer?.startPoint = CGPoint(x: 0.5, y: 0.0)
+        gradientLayer?.endPoint = CGPoint(x: 0.5, y: 1.0)
+        
+        gradientView.layer.addSublayer(gradientLayer!)
+        gradientView.bringSubviewToFront(dateLabel)
     }
     
     override func prepareForReuse() {
@@ -46,9 +44,11 @@ final class ImagesListCell: UITableViewCell {
     func configure(image: UIImage?, date: String, isLiked: Bool) {
         cellImage.image = image
         dateLabel.text = date
-        
+
         let likeImage = isLiked ? UIImage(named: "Active") : UIImage(named: "No Active")
         likeButton.setImage(likeImage, for: .normal)
+        
+        setupGradient()
     }
     
 }
